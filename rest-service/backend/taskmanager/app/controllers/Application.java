a9eb372b55070a7ce2eed1745b2589a1d6932b29
Filip Maelbrancke <filip@maelbrancke.net>
2012-07-17 00:00:48 +0200

    initial version of a play-framework-based taskmanager backend

---
+++ rest-service/backend/taskmanager/app/controllers/Application.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import models.Task;
+import play.*;
+import play.data.*;
+import play.mvc.*;
+
+import views.html.*;
+
+public class Application extends Controller {
+	
+	static Form<Task> taskForm = form(Task.class);
+
+	public static Result index() {
+		// return ok(index.render("Your new application is ready."));
+		return redirect(routes.Application.tasks());
+	}
+
+	public static Result tasks() {
+		return ok(
+				views.html.index.render(Task.all(), taskForm)
+				);
+	}
+
+	public static Result newTask() {
+		Form<Task> filledForm = taskForm.bindFromRequest();
+		if (filledForm.hasErrors()) {
+			return badRequest(
+					views.html.index.render(Task.all(), filledForm)
+					);
+		} else {
+			Task.create(filledForm.get());
+			return redirect(routes.Application.tasks());
+		}
+	}
+
+	public static Result deleteTask(Long id) {
+		Task.delete(id);
+		return redirect(routes.Application.tasks());
+	}
+
+}
---
+++ rest-service/backend/taskmanager/project/Build.scala
@@ -0,0 +1,20 @@
+import sbt._
+import Keys._
+import PlayProject._
+
+object ApplicationBuild extends Build {
+
+    val appName         = "taskmanager"
+    val appVersion      = "1.0-SNAPSHOT"
+
+    val appDependencies = Seq(
+      // Add your project dependencies here,
+      "com.github.twitter" % "bootstrap" % "2.0.4"
+    )
+
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+      // Add your own project settings here      
+      resolvers += "webjars" at "http://webjars.github.com/m2"
+    )
+
+}
